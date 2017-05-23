6b4b1ec6d10774d4a5a79f14e86da7a0eefbec8d
Pavel Karachev <pavelkarachev@ymail.com>
2017-05-23 18:49:35 +0300

    Task 03.01

---
+++ chepter_3/task1.js
@@ -0,0 +1,6 @@
+var words = ["fuck1", "fuck2", "fuck3"];
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var char = ["a", "b", "c", "d", "e", "f"];
+
+var string = words[Math.floor(Math.random() * words.length)] + " " + num[Math.floor(Math.random() * num.length)] + " " + char[Math.floor(Math.random() * char.length)];
+string;
